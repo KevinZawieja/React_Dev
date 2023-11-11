c3d6af9db55e9782f2db11c560bef21812663e03
KevinZawieja <kevin_zawieja@hotmail.de>
2023-11-11 22:33:41 +0100

    initalize

---
+++ src/ausgeben.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function ToDoList (){
+    return(
+    <ol>
+      <li>Learn React</li>
+      <li>Become a Developer</li>
+    </ol>)
+};
+  
+export default ToDoList;
